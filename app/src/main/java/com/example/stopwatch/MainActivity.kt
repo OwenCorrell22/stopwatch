daa8e611350f4147f75e40e652134015c55c440f
per2 <OwenCorrell32@gmail.com>
2022-09-12 08:59:29 -0700

    added methods onResume, onPause, onStop, onDestroy

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -15,11 +15,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart: ")
     }
+
+    override fun onResume() {
+        super.onResume()
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        setContentView(R.layout.activity_main)
+    }
+
 }
